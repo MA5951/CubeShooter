c13ee5d80edfec7a991289a626d0fefa96662249
GalDotan <gldotan@gmail.com>
2023-11-13 08:59:03 +0200

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/Cubeshotter.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ma5951.utils.subsystem.MotorSubsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Cubeshotter extends SubsystemBase implements MotorSubsystem{
+  /** Creates a new Cubeshotter. */
+  private CANSparkMax master;
+  private CANSparkMax slave;
+  private static Cubeshotter instance;
+
+  public Cubeshotter() {
+    master = new CANSparkMax(4, MotorType.kBrushless);
+    slave = new CANSparkMax(18, MotorType.kBrushless);
+
+    master.setIdleMode(IdleMode.kBrake);
+    slave.setIdleMode(IdleMode.kBrake);
+
+    master.setInverted(false);
+    slave.follow(master, true);
+  }
+
+
+  public static Cubeshotter getInstance() {
+    if (instance == null) {
+      instance = new Cubeshotter();
+    }
+    return instance;
+  }
+
+  @Override
+  public void setVoltage(double voltage) {
+      master.set(voltage / 12);
+  }
+
+  @Override
+    public boolean canMove() {
+        return true;
+    }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
